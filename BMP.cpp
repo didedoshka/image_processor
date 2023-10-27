6b209e28dad15512ac96ec70880e100445e4e304
dagorokhov <dagorokhov@edu.hse.ru>
2023-10-28 01:53:48 +0300

    change bmp

--- BMP.cpp
+++ BMP.cpp
@@ -1,55 +1,68 @@
 #include "BMP.hpp"
 
+#include <cstdint>
 #include <exception>
 #include <iostream>
 #include <fstream>
+#include <memory>
 #include <stdexcept>
 #include "pixel.hpp"
 
-size_t FourCharsToSizeT(const unsigned char* start) {
-    return static_cast<size_t>(start[0] + (start[1] << 8) + (start[2] << 16) + (start[3] << 24));
+
+BMP::BMP(const std::string& path) : path_(path) {
 }
 
-size_t TwoCharsToSizeT(const unsigned char* start) {
-    return static_cast<size_t>(start[0] + (start[1] << 8));
-}
-
-BMP::BMP(const std::string& path) {
-    std::ifstream input;
-    input = std::ifstream(path, std::ios::binary);
+Image BMP::GetImage() const {
+    std::ifstream input = std::ifstream(path_, std::ios::binary);
     if (input.fail()) {
         throw std::runtime_error{"Can't load input file."};
     }
 
-    data_ = std::vector<unsigned char>(std::istreambuf_iterator<char>(input), {});
-    size_t supposed_data_size = FourCharsToSizeT(&data_[2]);
-    if (data_.size() < 14 || data_.size() < 2 || data_[0] != 'B' || data_[1] != 'M' ||
-        data_.size() != supposed_data_size) {
-        throw std::runtime_error("Input BMP file is corrupted");
+    BMPHeader bmp_header;
+    input.read(reinterpret_cast<char*>(&bmp_header), sizeof(bmp_header));
+    if (input.fail()) {
+        throw std::runtime_error{"Input file is too small: Bitmap file header dosn't fit."};
     }
-    size_t supposed_dib_header_size = FourCharsToSizeT(&data_[14]);
-    if (supposed_dib_header_size != 40) {
-        throw std::runtime_error("DIB in input BMP file is not BITMAPINFOHEADER");
+
+    if (bmp_header.magic[0] != 'B' || bmp_header.magic[1] != 'M') {
+        throw std::runtime_error{"Input file is not BMP."};
     }
-    size_t bits_per_pixel = TwoCharsToSizeT(&data_[28]);
-    if (bits_per_pixel != 24) {
+    if (bmp_header.bitmap_offset != 54) {
+        throw std::runtime_error{"Input file uses wrong Bitmap file header or DIB header"};
+    }
+
+    BitmapInfoHeader bitmap_info_header;
+    input.read(reinterpret_cast<char*>(&bitmap_info_header), sizeof(bitmap_info_header));
+    if (input.fail()) {
+        throw std::runtime_error{"Input file is too small: BITMAPINFOHEADER dosn't fit."};
+    }
+
+    if (bitmap_info_header.header_size != 40) {
+        throw std::runtime_error("DIB header in input BMP file is not BITMAPINFOHEADER");
+    }
+    if (bitmap_info_header.color_planes_number != 1) {
+        throw std::runtime_error("The amount of color planes does not equal one");
+    }
+    if (bitmap_info_header.bits_per_pixel != 24) {
         throw std::runtime_error("Image is not 24 bits per pixel");
     }
-    size_t compression_method = FourCharsToSizeT(&data_[30]);
-    if (compression_method != 0) {
+    if (bitmap_info_header.compression != 0) {
         throw std::runtime_error("Image is compressed");
     }
-}
+    if (bitmap_info_header.colors_in_the_palette != 0 && bitmap_info_header.colors_in_the_palette != (1 << 24)) {
+        throw std::runtime_error("There are not 2^24 colors in the pallete");
+    }
 
-Image BMP::GetImage() const {
-    Image image(FourCharsToSizeT(&data_[18]), FourCharsToSizeT(&data_[22]));
-    size_t pixel_array_offset = FourCharsToSizeT(&data_[10]);
+    std::unique_ptr<uint8_t> bitmap(new uint8_t[bmp_header.file_size - bmp_header.bitmap_offset]);
+
+    Image image(bitmap_info_header.bitmap_width, bitmap_info_header.bitmap_height);
+    size_t pixel_array_offset = FourCharsToSizeT(&data[10]);
     size_t row_size = (24 * image.GetWidth() + 31) / 32 * 4;
     for (size_t row = 0; row < image.GetHeight(); row++) {
         for (size_t pixel_in_row = 0; pixel_in_row < image.GetWidth(); ++pixel_in_row) {
-            image.At(pixel_in_row, row) = PixelDouble(data_[pixel_array_offset + row * row_size + pixel_in_row * 3 + 2],
-                                                      data_[pixel_array_offset + row * row_size + pixel_in_row * 3 + 1],
-                                                      data_[pixel_array_offset + row * row_size + pixel_in_row * 3]);
+            image.At(pixel_in_row, row) = PixelDouble(data[pixel_array_offset + row * row_size + pixel_in_row * 3 + 2],
+                                                      data[pixel_array_offset + row * row_size + pixel_in_row * 3 + 1],
+                                                      data[pixel_array_offset + row * row_size + pixel_in_row * 3]);
         }
     }
     return image;
--- BMP.hpp
+++ BMP.hpp
@@ -1,8 +1,29 @@
 #pragma once
 
+#include <cstdint>
 #include <string>
 #include "image.hpp"
 
+struct BMPHeader {
+    uint8_t magic[2]{'B', 'M'};
+    uint32_t file_size{};
+    uint8_t depends_on_creator[4]{};
+    uint32_t bitmap_offset{54};
+};
+
+struct BitmapInfoHeader {
+    uint32_t header_size{40};
+    int32_t bitmap_width{};
+    int32_t bitmap_height{};
+    uint16_t color_planes_number{1};
+    uint16_t bits_per_pixel{24};
+    uint32_t compression{0};
+    uint32_t image_size{0};
+    int32_t horizontal_resolution{0};
+    int32_t vertical_resolution{0};
+    uint32_t colors_in_the_palette{0};
+    uint32_t important_colors{0};
+};
 
 class BMP {
 public:
