f132e658959be9159a81946c54a43181c49880b0
dagorokhov <dagorokhov@edu.hse.ru>
2023-10-28 02:42:39 +0300

    add DoubleToUInt8T

--- pixel.cpp
+++ pixel.cpp
@@ -1,4 +1,6 @@
 #include "pixel.hpp"
+#include <cmath>
+#include <cstdint>
 
 PixelDouble::PixelDouble() : red(0.0), green(0.0), blue(0.0) {
 }
@@ -10,6 +12,10 @@ PixelDouble::PixelDouble(uint8_t red, uint8_t green, uint8_t blue)
     : red(static_cast<double>(red) / UINT8_MAX),
       green(static_cast<double>(green) / UINT8_MAX),
       blue(static_cast<double>(blue) / UINT8_MAX) {
+}
+
+uint8_t PixelDouble::DoubleToUInt8T(double brightness) {
+    return static_cast<uint8_t>(std::round(std::max(std::min(brightness, 1.0), 0.0)) * UINT8_MAX);
 }
 
 std::ostream& operator<<(std::ostream& os, const PixelDouble& pixel) {
