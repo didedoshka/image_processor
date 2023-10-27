32a6ba957ea944da9f6a306e4b522abf07740c62
dagorokhov <dagorokhov@edu.hse.ru>
2023-10-27 22:49:26 +0300

    add functions to implement later

--- BMP.hpp
+++ BMP.hpp
@@ -10,6 +10,8 @@ class BMP {
 public:
     explicit BMP(const std::string& path);
     Image GetImage() const;
+    explicit BMP(const Image& image);
+    void Save(const std::string& path);
 
 private:
     std::vector<unsigned char> data_;
