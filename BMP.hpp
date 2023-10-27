e9423853a0694f4b0de83892bf89514b592110c7
dagorokhov <dagorokhov@edu.hse.ru>
2023-10-28 00:17:46 +0300

    new signature for BMP

--- BMP.hpp
+++ BMP.hpp
@@ -1,18 +1,15 @@
 #pragma once
 
 #include <string>
-#include <vector>
 #include "image.hpp"
 
-size_t FourCharsToSizeT(const unsigned char* start);
 
 class BMP {
 public:
     explicit BMP(const std::string& path);
     Image GetImage() const;
-    explicit BMP(const Image& image);
-    void Save(const std::string& path);
+    void Save(const Image& image);
 
 private:
-    std::vector<unsigned char> data_;
+    std::string path_;
 };
