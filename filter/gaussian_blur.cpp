756aaed5223fe227ba58ba8dde92e53ff3794803
dagorokhov <dagorokhov@edu.hse.ru>
2023-10-30 13:01:01 +0300

    fix reserving O(sigma^2) memory

--- filter/gaussian_blur.cpp
+++ filter/gaussian_blur.cpp
@@ -4,7 +4,7 @@
 
 GaussianBlurHorizontal::GaussianBlurHorizontal(Pixel::Channel sigma) {
     Image::SizeType border = std::ceil(3 * sigma);
-    matrix_.reserve((2 * border + 1) * (2 * border + 1));
+    matrix_.reserve(2 * border + 1);
     for (Image::SizeType delta_row = -border; delta_row <= border; ++delta_row) {
         matrix_.push_back({delta_row, 0,
                            std::sqrt(std::numbers::inv_pi_v<Pixel::Channel> / (2 * sigma * sigma)) *
@@ -14,7 +14,7 @@ GaussianBlurHorizontal::GaussianBlurHorizontal(Pixel::Channel sigma) {
 
 GaussianBlurVertical::GaussianBlurVertical(Pixel::Channel sigma) {
     Image::SizeType border = std::ceil(3 * sigma);
-    matrix_.reserve((2 * border + 1) * (2 * border + 1));
+    matrix_.reserve(2 * border + 1);
     for (Image::SizeType delta_column = -border; delta_column <= border; ++delta_column) {
         matrix_.push_back({ 0,delta_column,
                            std::sqrt(std::numbers::inv_pi_v<Pixel::Channel> / (2 * sigma * sigma)) *
